6517e9fe77227d498ad1d2ceb53b7d325167251a
Shibatch <lawnbone.tubist@gmail.com>
2024-05-13 10:02:17 +0900

    defaultの位置が末尾でなくても良いのか確かめるプログラム

---
+++ check_default.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int num = 0;
+
+	switch (num) {
+		default:
+			printf("default dayo.\n");
+			break;
+		case 0:
+			printf("case 0 dayo\n");
+			break;
+	}
+
+	return 0;
+}
